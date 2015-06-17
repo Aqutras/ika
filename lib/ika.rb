8ee26a7a500b7961b274d399fe3828b5c8830688
Shoya TANAKA <tanaka@aqte.net>
2015-06-18 00:13:36 +0900

    implement export in json format

--- lib/ika.rb
+++ lib/ika.rb
@@ -1,2 +1,15 @@
 module Ika
+  extend ActiveSupport::Concern
+
+  def export(options = self.class.reflections.keys)
+    to_json(include: options)
+  end
+
+  module ClassMethods
+    def export(options = reflections.keys)
+      includes(options).to_json(include: options)
+    end
+  end
 end
+
+ActiveRecord::Base.send(:include, Ika)
