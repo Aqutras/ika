9a17355817e3dc54f04efe73dd825cad3766d189
Shoya TANAKA <tanaka@aqte.net>
2015-06-18 11:01:58 +0900

    add spec for relation

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,13 +2,70 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   let(:user) { FactoryGirl.create(:user) }
-  it '.export' do
-    ret = "[{\"id\":1,\"email\":\"test@hoge.com\",\"name\":\"test name\",\"created_at\":#{user.created_at.to_json},\"updated_at\":#{user.updated_at.to_json}}]"
-    expect(User.export).to eq ret
+  let(:group) { FactoryGirl.create(:group) }
+  let(:group_user) { FactoryGirl.create(:group_user) }
+  context '.export' do
+    it 'get all relation' do
+      ret = [
+        {
+          'id' => 1,
+          'email' => 'user@mock.com',
+          'name' => 'user name',
+          'created_at' => JSON.parse(user.to_json)['created_at'],
+          'updated_at' => JSON.parse(user.to_json)['updated_at'],
+          'group_users' => [
+            {
+              'id' => 1,
+              'user_id' => 1,
+              'group_id' => 1,
+              'created_at' => JSON.parse(group_user.to_json)['created_at'],
+              'updated_at' => JSON.parse(group_user.to_json)['updated_at']
+            }
+          ],
+          'groups' => [
+            {
+              'id' => 1,
+              'domain_id' => 'domain id',
+              'name' => 'group name',
+              'created_at' => JSON.parse(group.to_json)['created_at'],
+              'updated_at' => JSON.parse(group.to_json)['updated_at']
+            }
+          ]
+        }
+      ]
+      expect(User.export).to match_json_expression(ret)
+    end
   end
 
-  it '#export' do
-    ret = "{\"id\":1,\"email\":\"test@hoge.com\",\"name\":\"test name\",\"created_at\":#{user.created_at.to_json},\"updated_at\":#{user.updated_at.to_json}}"
-    expect(User.first.export).to eq ret
+  context '#export' do
+    it 'get all relation' do
+      ret = {
+        'id' => 1,
+        'email' => 'user@mock.com',
+        'name' => 'user name',
+        'created_at' => JSON.parse(user.to_json)['created_at'],
+        'updated_at' => JSON.parse(user.to_json)['updated_at'],
+        'group_users' => [
+          {
+            'id' => 1,
+            'user_id' => 1,
+            'group_id' => 1,
+            'created_at' => JSON.parse(group_user.to_json)['created_at'],
+            'updated_at' => JSON.parse(group_user.to_json)['updated_at']
+          }
+        ],
+        'groups' => [
+          {
+            'id' => 1,
+            'domain_id' => 'domain id',
+            'name' => 'group name',
+            'created_at' => JSON.parse(group.to_json)['created_at'],
+            'updated_at' => JSON.parse(group.to_json)['updated_at']
+          }
+        ]
+      }
+
+      expect(User.first.export).to match_json_expression(ret)
+    end
   end
 end
